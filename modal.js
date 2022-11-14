e6a0c207f6c6f428eee172427905d281e60e89c5
Ulquio68 <107309990+Ulquio68@users.noreply.github.com>
2022-11-14 15:32:48 +0100

    session

--- modal.js
+++ modal.js
@@ -29,214 +29,167 @@ function closeModal() {
 }
 
 
-
-
+//prénom conditions
 let prenom_m = document.getElementById("prenom_manquant");
 let prenom_v = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?/;
-let prenom = document.getElementById("first");
 
-let valider = document.getElementsByClassName("btn-submit");
-valider[0].addEventListener('click', prenom_validation);
+function test_prenom() {
+  let prenom = document.getElementById("first");
 
-
-function prenom_validation(e) {
   if (prenom.validity.valueMissing) {
-    e.preventDefault();
-    prenom_m.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
-    prenom_m.style.color = "red";
-    prenom_m.style.fontSize = "16px";
-    prenom.style.border = "red 3px solid";
-  }
-  else if (prenom_v.test(prenom.value) == false) {
-    e.preventDefault();
-    prenom_m.textContent = "Prénom incorrect";
-    prenom_m.style.color = "orange";
-    prenom_m.style.fontSize = "16px";
-    prenom.style.border = "orange 3px solid";
+    prenom_m.classList.remove("hidden");
+    return false;
   }
   else {
+    prenom_m.classList.add("hidden");
+    return true;
   }
 }
 
-
+//nom conditions
 let nom_m = document.getElementById ("nom_manquant");
 let nom_v = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?/;
-let nom = document.getElementById("last");
 
-valider[0].addEventListener('click', nom_validation);
 
-function nom_validation(e) {
+function test_nom() {
+  let nom = document.getElementById("last");
+
   if (nom.validity.valueMissing) {
-    e.preventDefault();
-    nom_m.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom";
-    nom_m.style.color = "red";
-    nom_m.style.fontSize = "16px";
-    nom.style.border = "red 3px solid";
-  }
-  else if (nom_v.test(nom.value) == false) {
-    e.preventDefault();
-    nom_m.textContent = "Nom incorrect";
-    nom_m.style.color = "orange";
-    nom_m.style.fontSize = "16px";
-    nom.style.border = "orange 3px solid";
+    nom_m.classList.remove("hidden");
+    return false;
   }
   else {
+    nom_m.classList.add("hidden");
+    return true;
   }
 }
 
-
+//mail conditions
 let mail_m = document.getElementById("mail_manquant");
 let mail_v = /[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})/;
-let mail = document.getElementById("email");
 
-valider[0].addEventListener('click', mail_validation);
+function test_mail() {
+  let mail = document.getElementById("email");
 
-function mail_validation(e) {
   if (mail.validity.valueMissing) {
-    e.preventDefault();
-    mail_m.textContent = "Veuillez entrer votre e-mail";
-    mail_m.style.color = "red";
-    mail_m.style.fontSize = "16px";
-    mail.style.border = "red 3px solid";
-  }
-  else if (mail_v.test(mail.value) == false) {
-    e.preventDefault();
-    mail_m.textContent = "E-mail incorrect";
-    mail_m.style.color = "orange";
-    mail_m.style.fontSize = "16px";
-    mail.style.border = "orange 3px solid";
+    mail_m.classList.remove("hidden");
+    return false;
   }
   else {
+    mail_m.classList.add("hidden");
+    return true;
   }
 }
 
-
+//date de naissance conditions
 let date_m = document.getElementById ("date_manquant");
 let date_v = /[0-9]/;
-let date = document.getElementById("birthdate");
 
-valider[0].addEventListener('click', date_validation);
+function test_date() {
+  let date = document.getElementById("birthdate");
 
-function date_validation(e) {
   if (date.validity.valueMissing) {
-    e.preventDefault();
-    date_m.textContent = "Vous devez entrer votre date de naissance";
-    date_m.style.color = "red";
-    date_m.style.fontSize = "16px";
-    date.style.border = "red 3px solid";
-  }
-  else if (date_v.test(date.value) == false) {
-    e.preventDefault();
-    date_m.textContent = "Format incorrect";
-    date_m.style.color = "orange";
-    date_m.style.fontSize = "16px";
-    date.style.border = "orange 3px solid";
+    date_m.classList.remove("hidden");
+    return false;
   }
   else {
+    date_m.classList.add("hidden");
+    return true;
   }
 }
 
-
+//reponse conditions
 let reponse_m = document.getElementById ("reponse_manquant");
 let reponse_v = /[0-9]/;
-let reponse = document.getElementById("quantity");
 
-valider[0].addEventListener('click', reponse_validation);
+function test_reponse() {
+  let reponse = document.getElementById("quantity");
 
-function reponse_validation(e) {
   if (reponse.validity.valueMissing) {
-    e.preventDefault();
-    reponse_m.textContent = "Veuillez entrer un nombre";
-    reponse_m.style.color = "red";
-    reponse_m.style.fontSize = "16px";
-    reponse.style.border = "red 3px solid";
-  }
-  else if (reponse_v.test(reponse.value) == false) {
-    e.preventDefault();
-    reponse_m.textContent = "Format incorrect";
-    reponse_m.style.color = "orange";
-    reponse_m.style.fontSize = "16px";
-    reponse.style.border = "orange 3px solid";
+    reponse_m.classList.remove("hidden");
+    return false;
   }
   else {
+    reponse_m.classList.add("hidden");
+    return true;
   }
 }
 
 
 
-
+//choice conditions
 let choice_m = document.getElementById("choice_manquant");
 
-valider[0].addEventListener('click', choice);
 
-
-let choice1 = document.getElementById("location1").checked;
-let choice2 = document.getElementById("location2").checked;
-let choice3 = document.getElementById("location3").checked;
-let choice4 = document.getElementById("location4").checked;
-let choice5 = document.getElementById("location5").checked;
-let choice6 = document.getElementById("location6").checked;
-
-
-  function choice() {
-
+  function test_choice() {
+    let choice1 = document.getElementById("location1").checked;
+    let choice2 = document.getElementById("location2").checked;
+    let choice3 = document.getElementById("location3").checked;
+    let choice4 = document.getElementById("location4").checked;
+    let choice5 = document.getElementById("location5").checked;
+    let choice6 = document.getElementById("location6").checked;
     let valid = false;
 
     if((choice1 == false) && (choice2 == false) && 
     (choice3 == false) && (choice4 == false)
     && (choice5 == false) && (choice6 == false)) {
-      choice_m.textContent = "Vous devez choisir une option";
-      choice_m.style.color = "red";
-      choice_m.style.fontSize = "16px";
+
+      choice_m.classList.remove("hidden");
     }
 
     else
     {
       valid = true;
+      choice_m.classList.add("hidden");
     }
 
     return valid;
   }
 
 
-
+//checkbox conditions
 let checkbox_m = document.getElementById("checkbox_manquant");
-let checkbox_input = document.getElementById("checkbox1").checked;
 
-valider[0].addEventListener('click', checkbox);
 
-function checkbox () {
+function test_checkbox () {
+  let checkbox_input = document.getElementById("checkbox1").checked;
   let valid = false;
 
   if (checkbox_input == false) {
     
-    checkbox_m.textContent = "Vous devez vérifier que vous acceptez les termes et conditions";
-    checkbox_m.style.color = "red";
-    checkbox_m.style.fontSize = "16px";
-
+    checkbox_m.classList.remove("hidden");
   }
     else
     {
       valid = true;
+      checkbox_m.classList.add("hidden");
     }
 
     return valid;
   }
 
+  
+// validation formulaire
 
+const formulaire = document.getElementById("formulaire");
+const body = document.getElementsByClassName("modal-body");
+formulaire.addEventListener('submit', validation_full);
+let modal_validé = document.getElementsByClassName("modal-validé");
 
-let validation_totale = document.getElementsByClassName("formu_close");
+function validation_full(e) {
+  e.preventDefault();
+  const date_valid = test_date();
+  const choice_valid = test_choice();
+  const checkbox_valid = test_checkbox();
+  const reponse_valid = test_reponse();
+  const mail_valid = test_mail();
+  const nom_valid = test_nom();
+  const prenom_valid = test_prenom();
 
-validation_totale.addEventListener('click', validation_full);
+  if (date_valid == true && choice_valid == true && checkbox_valid == true && reponse_valid == true && mail_valid == true && nom_valid == true && prenom_valid == true) {
 
-function validation_full() {
-  if (valider[0] == true) {
-    validation_totale.textContent = "Merci pour votre inscription";
+    formulaire.style.display = "none";
+    modal_validé.classList.remove("hidden");
   }
-}
-
-
-function validate(stop) {
-  stop.preventdefault();
 }
 
